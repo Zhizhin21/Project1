fd77395cb65b2837880b8463a094a771755c8012
Zhizhin21 <36304132+Zhizhin21@users.noreply.github.com>
2019-07-07 22:04:20 +0100

    Add files via upload
    Doing diff dates

---
+++ Scraper_Gen_Dates.py
@@ -0,0 +1,20 @@
+import twint
+from multiprocessing import Pool
+def twitter_search(date):
+	c = twint.Config()
+	words = ["Brexit", 'Eureferendum', 'EUreferendum', 'EU Referendum', 'brexit', 'Euref', 'BREXIT']
+	c.Search = words 
+	c.Store_csv = True
+	c.Since = "2016-06-2{}".format(date)
+	c.Until = "2016-06-2{}".format(date+1)
+	c.Format = '| Date: {date} | Time: {time} |'
+	#"Tweet id: {id} | Date: {date} | Time: {time} | Username: {username} | User_id: {user_id} | Tweet: {tweet} | Hashtags: {hashtags} | Replies: {replies} | Retweets: {retweets} | Likes: {likes} | Location: {location}"
+	c.Output = "brexit_results_{}.csv".format(date)
+	c.Location = True
+	twint.run.Search(c)
+
+
+
+dates = list(range(1,7))
+p = Pool(4)
+p.map(twitter_search, dates)
